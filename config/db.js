76c22e75ec5252352e090542a6f5a0ab42b2289f
day-kia <sazkiarhmdinaa@gmail.com>
2026-06-15 12:13:00 +0700

    Add MySQL port 3307

--- config/db.js
+++ config/db.js
@@ -3,6 +3,7 @@ require('dotenv').config();
 
 const db = mysql.createPool({
   host: process.env.DB_HOST,
+  port:3307,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME
